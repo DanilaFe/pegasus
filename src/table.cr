45ef7c08cd8cd1f81b26a40758182d7dd8690d35
Danila Fedorin <danila.fedorin@gmail.com>
2017-12-31 16:03:32 -0800

    Add table generating function.

---
+++ src/table.cr
@@ -0,0 +1,20 @@
+module Pegasus
+  module Nfa
+    class Nfa
+      def table
+        table = [Array.new(255, 0_i64)]
+        @states.each do |state|
+          empty_table = Array.new(255, 0_i64)
+          state.transitions
+              .select(&.is_a?(ByteTransition))
+              .map(&.as(ByteTransition))
+              .each do |transition|
+            empty_table[transition.byte] = transition.other.id + 1
+          end
+          table << empty_table
+        end
+        return table
+      end
+    end
+  end
+end

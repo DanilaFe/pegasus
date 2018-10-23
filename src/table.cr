2e15ae635c17c134d60c5da5424e13da50423dee
Danila Fedorin <danila.fedorin@gmail.com>
2018-10-22 20:32:42 -0700

    Output lookup table containing final states.

--- src/table.cr
+++ src/table.cr
@@ -1,7 +1,11 @@
 module Pegasus
   module Nfa
     class Nfa
-      def table
+      def final_table
+        return [0] + @states.map { |s| s.final_id || 0 }
+      end
+
+      def state_table
         table = [Array.new(255, 0_i64)]
         @states.each do |state|
           empty_table = Array.new(255, 0_i64)
@@ -9,7 +13,6 @@ module Pegasus
               .select(&.is_a?(ByteTransition))
               .map(&.as(ByteTransition))
               .each do |transition|
-            puts transition.other.id
             empty_table[transition.byte] = transition.other.id + 1
           end
           table << empty_table

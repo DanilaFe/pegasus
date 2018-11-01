8c118c32f23f3e7f8e5597756f8687cda09fcef6
Danila Fedorin <danila.fedorin@gmail.com>
2018-10-31 18:49:36 -0700

    Add a state transition table. This should conclude the LALR parser.

--- src/table.cr
+++ src/table.cr
@@ -26,17 +26,42 @@ module Pegasus
           item.body.select(&.is_a?(Terminal)).max_of?(&.id) || 0_i64
         end || 0_i64
 
-        table = Array.new(@states.size + 1) { |inded| Array.new(max_terminal + 1, 0_i64) }
+        # +1 for potential -1, +1 since terminal IDs start at 0.
+        table = Array.new(@states.size + 1) { |inded| Array.new(max_terminal + 1 + 1, 0_i64) }
         @states.each do |state|
           done_items = state.data.select &.done?
           done_items.each do |item|
             item.lookahead.each do |terminal|
-              puts terminal.id
-              table[state.id + 1][terminal.id + 1] = @items.index(item.item).not_nil!.to_i64
+              table[state.id + 1][terminal.id + 1] = @items.index(item.item).not_nil!.to_i64 + 1
             end
           end
         end
         
+        return table
+      end
+      
+      def state_table
+        max_terminal = @items.max_of? do |item|
+          item.body.select(&.is_a?(Terminal)).max_of?(&.id) || 0_i64
+        end || 0_i64
+
+        max_nonterminal = @items.max_of? do |item|
+          Math.max(item.head.id, item.body.select(&.is_a?(Nonterminal)).max_of?(&.id) || 0_i64)
+        end || 0_i64
+
+        # +1 for potential -1 in terminal, +1 + 1 because both terminal and nonterminals start at 0.
+        table = Array.new(@states.size + 1) { |i| Array.new(max_nonterminal + max_terminal + 1 + 1 + 1, 0_i64) }
+        @states.each do |state|
+          state.transitions.each do |token, to|
+            case token
+            when Terminal
+              table[state.id + 1][token.id + 1] = to.id + 1
+            when Nonterminal
+              table[state.id + 1][token.id + 1 + 1 + max_terminal] = to.id + 1
+            end
+          end
+        end
+
         return table
       end
     end

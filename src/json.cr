120e2cc80d3c70c4d2e090aaadc129fd356593af
Danila Fedorin <danila.fedorin@gmail.com>
2018-11-02 14:36:30 -0700

    Remove setters being generated by JSON.

--- src/json.cr
+++ src/json.cr
@@ -4,35 +4,35 @@ module Pegasus
   module Pda
     class Terminal
       JSON.mapping(
-        id: { type: Int64, key: "terminal_id" }
+        id: { type: Int64, key: "terminal_id", setter: false }
       )
     end
 
     class Nonterminal
       JSON.mapping(
-        id: { type: Int64, key: "nonterminal_id" }
+        id: { type: Int64, key: "nonterminal_id", setter: false }
       )
     end
 
     class Item
       JSON.mapping(
-        head: Nonterminal,
-        body: Array(Terminal | Nonterminal)
+        head: { type: Nonterminal, setter: false },
+        body: { type: Array(Terminal | Nonterminal), setter: false }
       )
     end
   end
   module Language
     class LanguageData
       JSON.mapping(
-        lex_state_table: Array(Array(Int64)),
-        lex_final_table: Array(Int64),
-        parse_state_table: Array(Array(Int64)),
-        parse_action_table: Array(Array(Int64)),
+        lex_state_table: { type: Array(Array(Int64)), setter: false },
+        lex_final_table: { type: Array(Int64), setter: false },
+        parse_state_table: { type: Array(Array(Int64)), setter: false },
+        parse_action_table: { type: Array(Array(Int64)), setter: false },
 
-        terminals: Hash(String, Pegasus::Pda::Terminal),
-        nonterminals: Hash(String, Pegasus::Pda::Nonterminal),
-        items: Array(Pegasus::Pda::Item),
-        max_terminal: Int64
+        terminals: { type: Hash(String, Pegasus::Pda::Terminal), setter: false },
+        nonterminals: { type: Hash(String, Pegasus::Pda::Nonterminal), setter: false },
+        items: { type: Array(Pegasus::Pda::Item), setter: false },
+        max_terminal: { type: Int64, setter: false }
       )
     end
   end

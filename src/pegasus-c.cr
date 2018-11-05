d6304ffa19109173310a151db5a27876c1f528d3
Danila Fedorin <danila.fedorin@gmail.com>
2018-11-05 15:24:09 -0800

    Implement basic C output (no command line parameters yet).

---
+++ src/pegasus-c.cr
@@ -0,0 +1,17 @@
+require "./language_def.cr"
+require "./json.cr"
+require "option_parser"
+require "ecr"
+
+module Pegasus
+  module Language
+    class LanguageData
+      def output(io)
+        ECR.embed "src/pegasus-c-template.ecr", io
+      end
+    end
+  end
+end
+
+data = Pegasus::Language::LanguageData.from_json STDIN
+data.output(STDOUT)

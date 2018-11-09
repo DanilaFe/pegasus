19446ac7d10006b94e76ec22d7fd715811bb78c6
Danila Fedorin <danila.fedorin@gmail.com>
2018-11-09 12:22:24 -0800

    Add helper methods and write more specs.

--- spec/pegasus_spec.cr
+++ spec/pegasus_spec.cr
@@ -1,5 +1,23 @@
 require "./spec_helper"
 
+def nonterminal(id)
+  Pegasus::Pda::Nonterminal.new id.to_i64
+end
+
+def terminal(id)
+  Pegasus::Pda::Terminal.new id.to_i64
+end
+
+def body(*elements)
+  array = [] of Pegasus::Pda::Element
+  array.concat elements.to_a
+  return array
+end
+
+def item(head, body)
+  Pegasus::Pda::Item.new head, body
+end
+
 describe Pegasus::Automaton do
   describe "#initialize" do
     it "Starts at state 0" do
@@ -109,18 +127,18 @@ describe Pegasus::Pda::Grammar do
     it "Handles empty grammars" do
       grammar = Pegasus::Pda::Grammar.new [] of Pegasus::Pda::Terminal,
         [] of Pegasus::Pda::Nonterminal
-      pda = grammar.create_lr_pda Pegasus::Pda::Nonterminal.new 0_i64
+      pda = grammar.create_lr_pda nonterminal 0
       pda.states.size.should eq 1
       pda.states.first.transitions.size.should eq 0
       pda.states.first.data.size.should eq 0
     end
 
     it "Handles grammars with one rule" do
-      grammar = Pegasus::Pda::Grammar.new [ Pegasus::Pda::Terminal.new 0_i64 ],
-        [ Pegasus::Pda::Nonterminal.new 0_i64 ]
-      grammar.add_item Pegasus::Pda::Item.new head: Pegasus::Pda::Nonterminal.new(0_i64),
-        body: [ Pegasus::Pda::Terminal.new 0_i64 ] of Pegasus::Pda::Element
-      pda = grammar.create_lr_pda Pegasus::Pda::Nonterminal.new 0_i64
+      grammar = Pegasus::Pda::Grammar.new [ terminal 0 ],
+        [ nonterminal 0 ]
+      grammar.add_item item head: nonterminal(0),
+        body: body terminal(0)
+      pda = grammar.create_lr_pda nonterminal 0
       pda.states.size.should eq 2 # Start + with item shifted over
 
       start_state = pda.states.find(&.id.==(0)).not_nil!
@@ -129,14 +147,15 @@ describe Pegasus::Pda::Grammar do
     end
 
     it "Handles grammars with epsilon-moves" do
-      terminals = [ Pegasus::Pda::Terminal.new 0_i64 ]
-      nonterminals = [ Pegasus::Pda::Nonterminal.new(0_i64),
-        Pegasus::Pda::Nonterminal.new(1_i64) ]
+      terminals = [ terminal 0 ]
+      nonterminals = [ nonterminal(0), nonterminal(1) ]
+
       grammar = Pegasus::Pda::Grammar.new terminals, nonterminals
-      grammar.add_item Pegasus::Pda::Item.new head: nonterminals[0],
-        body: [ nonterminals[1] ] of Pegasus::Pda::Element
-      grammar.add_item Pegasus::Pda::Item.new head: nonterminals[1],
-        body: [ terminals[0] ] of Pegasus::Pda::Element
+      grammar.add_item item head: nonterminals[0],
+        body: body nonterminals[1]
+      grammar.add_item item head: nonterminals[1],
+        body: body terminals[0]
+
       pda = grammar.create_lr_pda nonterminals[0]
       pda.states.size.should eq 3
 
@@ -167,35 +186,71 @@ describe Pegasus::Pda::Grammar do
   describe "#create_lalr_pda" do
     it "Meges states with duplicate bodies" do
       # This grammar is taken from grammars/modern_compiler_design.grammar
-      t_x = Pegasus::Pda::Terminal.new(0_i64)
-      t_b = Pegasus::Pda::Terminal.new(1_i64)
-      t_a = Pegasus::Pda::Terminal.new(2_i64)
-      terminals = [
-        t_x, t_b, t_a
-      ]
-      s = Pegasus::Pda::Nonterminal.new(0_i64)
-      a = Pegasus::Pda::Nonterminal.new(1_i64)
-      b = Pegasus::Pda::Nonterminal.new(2_i64)
-      nonterminals = [
-        s, a, b
-      ]
+      t_x = terminal(0)
+      t_b = terminal(1)
+      t_a = terminal(2)
+      terminals = [ t_x, t_b, t_a ]
+
+      s = nonterminal 0
+      a = nonterminal 1
+      b = nonterminal 2
+      nonterminals = [ s, a, b ]
 
       grammar = Pegasus::Pda::Grammar.new terminals, nonterminals
-      grammar.add_item Pegasus::Pda::Item.new head: s,
-        body: [ a ] of Pegasus::Pda::Element
-      grammar.add_item Pegasus::Pda::Item.new head: s,
-        body: [ t_x, t_b ] of Pegasus::Pda::Element
-      grammar.add_item Pegasus::Pda::Item.new head: a,
-        body: [ t_a, a, t_b ] of Pegasus::Pda::Element
-      grammar.add_item Pegasus::Pda::Item.new head: a,
-        body: [ b ] of Pegasus::Pda::Element
-      grammar.add_item Pegasus::Pda::Item.new head: b,
-        body: [ t_x ] of Pegasus::Pda::Element
+      grammar.add_item item head: s,
+        body: body a
+      grammar.add_item item head: s,
+        body: body t_x, t_b
+      grammar.add_item item head: a,
+        body: body t_a, a, t_b
+      grammar.add_item item head: a,
+        body: body b
+      grammar.add_item item head: b,
+        body: body t_x
     
       lr_pda = grammar.create_lr_pda s
       lalr_pda = grammar.create_lalr_pda lr_pda
       lr_pda.states.size.should eq 13
       lalr_pda.states.size.should eq 9
+    end
+  end
+end
+
+describe Pegasus::Pda::DottedItem do
+  describe "#next_item!" do
+    it "Advances the index when possible" do
+      new_item = item head: nonterminal(0),
+        body: body terminal(0), terminal(0)
+      dotted_item = Pegasus::Pda::DottedItem.new new_item, index: 0_i64
+      dotted_item.next_item!
+      dotted_item.index.should eq 1
+      dotted_item.next_item!
+      dotted_item.index.should eq 2
+    end
+
+    it "Raises when already at the end" do
+      new_item = item head: nonterminal(0),
+        body: body terminal(0), terminal(0)
+      dotted_item = Pegasus::Pda::DottedItem.new new_item, index: 2_i64
+      expect_raises(Exception) do
+        dotted_item.next_item!
+      end
+    end
+  end
+
+  describe "#done?" do
+    it "Returns false when dot is not past the last element" do
+      new_item = item head: nonterminal(0),
+        body: body terminal(0), terminal(0)
+      dotted_item = Pegasus::Pda::DottedItem.new new_item, index: 0_i64
+      dotted_item.done?.should be_false
+    end
+
+    it "Returns true when dot is just after the last element" do
+      new_item = item head: nonterminal(0),
+        body: body terminal(0), terminal(0)
+      dotted_item = Pegasus::Pda::DottedItem.new new_item, index: 2_i64
+      dotted_item.done?.should be_true
     end
   end
 end

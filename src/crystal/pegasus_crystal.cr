9977f32a99cd6e8b479689ec29b8c0b547156ae1
Danila Fedorin <danila.fedorin@gmail.com>
2018-11-11 00:18:01 -0800

    Add initial Crystal-based back end.

---
+++ src/crystal/pegasus_crystal.cr
@@ -0,0 +1,38 @@
+require "../pegasus/language_def.cr"
+require "../pegasus/json.cr"
+require "option_parser"
+require "ecr"
+
+module Pegasus
+  module Language
+    class LanguageData
+      def output(io, prefix)
+        ECR.embed "src/crystal/pegasus_crystal_template.ecr", io
+      end
+    end
+  end
+end
+
+prefix = "Pegasus::Generated"
+file_name = "parser"
+stdout = false
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: pegasus-crystal [arguments]"
+  parser.on("-S", "--standard-out", "Combines the header and implementation files, and prints to standard out") { stdout = true }
+  parser.on("-P PREFIX", "--prefix PREFIX", "Specify the prefix for generated code") do |p| 
+    prefix = p
+  end
+  parser.on("-f FILE", "--file-name=FILE", "Sets output file name") { |file| file_name = file }
+  parser.on("-h", "--help", "Displays this message") { puts parser }
+end
+
+data = Pegasus::Language::LanguageData.from_json STDIN
+if stdout
+  data.output STDOUT, prefix
+else
+  file = File.open(file_name + ".cr", mode = "w")
+  data.output file, prefix
+  file.close
+end
+

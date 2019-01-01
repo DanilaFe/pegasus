c51ba0fde1afea1bb7ccc1941ab2d5ed24052af0
Danila Fedorin <danila.fedorin@gmail.com>
2018-12-31 19:34:55 -0800

    Rewrite some specs to check spacing and bad grammars better.

--- spec/language_spec.cr
+++ spec/language_spec.cr
@@ -8,35 +8,79 @@ describe Pegasus::Language::LanguageDefinition do
       end
     end
 
-    it "Errors on just a nonterminal without a body" do
+    it "Errors on just a rule without a body" do
       expect_raises(Pegasus::Error::GrammarException) do
-        language = Pegasus::Language::LanguageDefinition.new %(S);
+        language = Pegasus::Language::LanguageDefinition.new %(rule S);
       end
     end
 
-    it "Errors on just a nontemrinal and an equals sign" do
+    it "Errors on just a token without a body" do
       expect_raises(Pegasus::Error::GrammarException) do
-        language = Pegasus::Language::LanguageDefinition.new %(S=);
+        language = Pegasus::Language::LanguageDefinition.new %(rule S);
+      end
+    end
+    
+    it "Errors on just a rule with an equals sign, but no body" do
+      expect_raises(Pegasus::Error::GrammarException) do
+        language = Pegasus::Language::LanguageDefinition.new %(rule S = );
       end
     end
 
-    it "Errors on a production not ending in a semicolon" do
+    it "Errors on just a token with an equals sign, but no body" do
       expect_raises(Pegasus::Error::GrammarException) do
-        language = Pegasus::Language::LanguageDefinition.new %(S="h")
+        language = Pegasus::Language::LanguageDefinition.new %(token S = );
       end
     end
 
-    it "Errors on a production not ending in a semicolon, when another production follows" do
+    it "Errors on a token not ending in a semicolon, when another rule follows" do
       expect_raises(Pegasus::Error::GrammarException) do
-        language = Pegasus::Language::LanguageDefinition.new %(S=expr\nexpr="h";)
+        language = Pegasus::Language::LanguageDefinition.new %(token t = /t/\nrule expr = h;)
       end
     end
+
+    it "Errors on a rule not ending in a semicolon, when another rule follows" do
+      expect_raises(Pegasus::Error::GrammarException) do
+        language = Pegasus::Language::LanguageDefinition.new %(rule S = expr\nrule expr = h;)
+      end
+    end
+
 
     it "Correctly parses a single rule with a single terminal or nonterminal" do
       language = Pegasus::Language::LanguageDefinition.new %(rule S = h;)
       language.tokens.size.should eq 0
       language.rules.size.should eq 1
       language.rules["S"]?.should eq [ [ "h" ] ]
+    end
+
+    it "Correctly handles whitespace between the token / rule keyword and the identifier" do
+      language = Pegasus::Language::LanguageDefinition.new %(token   \n  t   \n  = /t/;rule   \n  S  \n   = t;)
+      language.tokens.size.should eq 1
+      language.tokens["t"]?.should eq "t"
+      language.rules.size.should eq 1
+      language.rules["S"]?.should eq [ [ "t" ] ]
+    end
+
+    it "Correctly handles whitespace around the equals sign" do
+      language = Pegasus::Language::LanguageDefinition.new %(token t   \n =    /t/;rule S   \n =    \nt;)
+      language.tokens.size.should eq 1
+      language.tokens["t"]?.should eq "t"
+      language.rules.size.should eq 1
+      language.rules["S"]?.should eq [ [ "t" ] ]
+    end
+
+    it "Correctly handles whitespace around the semicolon" do
+      language = Pegasus::Language::LanguageDefinition.new %(token t = /t/   \n  ;   \n   rule S = t  \n  ;    \n)
+      language.tokens.size.should eq 1
+      language.tokens["t"]?.should eq "t"
+      language.rules.size.should eq 1
+      language.rules["S"]?.should eq [ [ "t" ] ]
+    end
+
+    it "Correctly handles whitespace between rule identifiers" do
+      language = Pegasus::Language::LanguageDefinition.new %(rule S = hello   \n  goodbye   \n  |   \n   world;)
+      language.tokens.size.should eq 0
+      language.rules.size.should eq 1
+      language.rules["S"]?.should eq [ [ "hello", "goodbye" ], [ "world" ] ]
     end
 
     it "Correctly parses a single token declaration" do
